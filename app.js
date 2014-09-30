a61d02bcc7ddeaff759f35e61f73bf8726cda535
Victor Chong <victor.chong@linaro.org>
2014-10-01 01:00:21 +0900

    Switch to hello jenkins world

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello jenkins world');
 });
 
 app.listen(process.env.PORT || 5000);
