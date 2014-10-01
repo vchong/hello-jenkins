48e8b98d63715882bf078f5933310590d180c305
vchong <vchong@vclinm.local>
2014-10-01 18:38:25 +0900

    Test Jenkins build trigger by git push

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('respond with hello world', function(done) {
+  it('respond with hello jenkins world', function(done) {
     request(app).get('/').expect('hello jenkins world', done);
   });
 });
